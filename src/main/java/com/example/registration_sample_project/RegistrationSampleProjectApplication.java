275cc6ddee3c4e8a17941c91864b56b26d75e76b
Thurain21 <thurainkosg@gmail.com>
2024-09-14 20:20:28 +0800

    add all files

---
+++ src/main/java/com/example/registration_sample_project/RegistrationSampleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.registration_sample_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationSampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationSampleProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/registration_sample_project/RegistrationSampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.registration_sample_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationSampleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
